1120f1f859637b217a1c22c740c636886b3e7317
icke758 <85443220+icke758@users.noreply.github.com>
2024-05-08 23:03:48 -0300

    Singleton

---
+++ Singleton/LoadBalancer.cs
@@ -0,0 +1,39 @@
+﻿namespace Singleton;
+public class LoadBalancer
+{
+    static LoadBalancer? instance;
+    readonly List<string> servers = new();
+
+    private static readonly object locker = new();
+
+    protected LoadBalancer()
+    {
+        servers.Add("ServerI");
+        servers.Add("ServerII");
+        servers.Add("ServerIII");
+        servers.Add("ServerIV");
+        servers.Add("ServerV");
+    }
+
+    public static LoadBalancer GetLoadBalancer()
+    {
+        if (instance == null)
+        {
+            lock (locker)
+            {
+                instance ??= new LoadBalancer();
+            }
+        }
+        return instance;
+    }
+
+    public string Server
+    {
+        get
+        {
+            Random random = new();
+            int r = random.Next(servers.Count);
+            return servers[r].ToString();
+        }
+    }
+}
